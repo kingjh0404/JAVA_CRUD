ff49f8297b800e858d2af3cd13ca1651a7c5c13e
king0404 <xhakxh147@handong.ac.kr>
2022-09-06 17:39:51 +0900

    first commit

--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -13,7 +13,7 @@ public class WordManager {
 	public int selectMenu() {
 		System.out.println("*** 영단어 마스터 ***\n\n"
 				+ "**********************\n"
-				+ "1. 모든 단어 보기\n"
+				+ "1. 모든 단어 보기 \n"
 				+ "2. 수준별 단어 보기\n"
 				+ "3. 단어 검색\n"
 				+ "4. 단어 추가\n"
