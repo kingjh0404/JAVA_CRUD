2d17662fe3d148ca0ab8418b1bed85f39a1ed567
king0404 <xhakxh147@handong.ac.kr>
2022-09-16 16:38:47 +0900

    space update

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -180,7 +180,7 @@ public class WordCRUD implements ICRUD{
 
 	public void searchWord() {
 		// TODO Auto-generated method stub
-		System.out.print("=> 원하는 단어는? ");
+		System.out.print("=> 원하는 단어는?   ");
 		String keyword = s.next();
 		listAll(keyword);
 		
